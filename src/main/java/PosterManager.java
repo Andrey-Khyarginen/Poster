62eb29e566ddd337315ea5a2a11d4564fc9e3564
Andrey Kharginen <kut13@inbox.ru>
2023-02-12 18:47:15 +0400

    changed code

--- src/main/java/PosterManager.java
+++ src/main/java/PosterManager.java
@@ -1,6 +1,6 @@
 public class PosterManager {
     private String[] movies = new String[0];
-    public int limit;
+    private int limit;
 
     public PosterManager() {
         this.limit = 10;
@@ -12,7 +12,7 @@ public class PosterManager {
 
     public void add(String movie) {
         String[] tmp = new String[movies.length + 1];
-        for (int i = 0; i < tmp.length; i++) {
+        for (int i = 0; i < tmp.length - 1; i++) {
             tmp[i] = movies[i];
         }
         tmp[tmp.length - 1] = movie;
@@ -25,12 +25,12 @@ public class PosterManager {
 
     public String[] findLast() {
         int resultLength;
-        if (movies.length < 10) {
+        if (movies.length < limit) {
             resultLength = movies.length;
         } else {
-            resultLength = 10;
+            resultLength = limit;
         }
-        String[] tmp = new String[10];
+        String[] tmp = new String[resultLength];
         for (int i = 0; i < tmp.length; i++) {
             tmp[i] = movies[movies.length - 1 - i];
         }
